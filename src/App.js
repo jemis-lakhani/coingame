2b0a2cf14d3a40ab52ec2362f9c7110a76b2154e
jemis-lakhani <jemislakhani.tech@gmail.com>
2024-02-17 15:54:14 +0530

    - File name revert

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import WaitingRoom from "./components/WaitingRoom1";
+import WaitingRoom from "./components/WaitingRoom";
 import Header from "./components/Header";
-import Footer from "./components/footer1";
+import Footer from "./components/Footer";
 
 function App() {
   return (
