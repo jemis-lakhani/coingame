65c1a0d8c889753993e93efc39fc17434652c9a0
jemis-lakhani <jemislakhani.tech@gmail.com>
2024-02-17 15:53:18 +0530

    - File name update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import WaitingRoom from "./components/WaitingRoom";
+import WaitingRoom from "./components/WaitingRoom1";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Footer from "./components/footer1";
 
 function App() {
   return (
